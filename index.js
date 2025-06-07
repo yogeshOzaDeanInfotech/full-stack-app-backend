d8acab77fa01860e7b9f5a8eb2a05327743bd2f2
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-06-07 10:01:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 5000;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
+app.listen(port, () => {
+    console.log(`App is started on port ${port}`);
+});
+
+let users = [
+    { name: "Yogesh", email: "Yogesh@gmail.com" },
+    { name: "virat", email: "virat@gmail.com" },
+    { name: "dhoni", email: "dhoni@gmail.com" },
+    { name: "deepak", email: "deepak@gmail.com" },
+];
+
+app.get("/api/users", async (req, res) => {
+    res.status(200).json({
+        status: true,
+        users,
+    });
+});
